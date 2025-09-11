dec7d6a18e35f40174250ca04d5df1a3f1005bbb
Andreas Nilsen <124470447+CyberNilsen@users.noreply.github.com>
2025-09-11 16:30:26 +0200

    Program now gets cenetered

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
-using Avalonia.Controls;
 using AkademiTrack.ViewModels;
+using Avalonia;
+using Avalonia.Controls;
+using System;
 
 namespace AkademiTrack.Views
 {
@@ -9,6 +11,37 @@ namespace AkademiTrack.Views
         {
             InitializeComponent();
             DataContext = new MainWindowViewModel();
+
+            // Set initial position to center - this will be refined after loading
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void OnWindowLoaded(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            CenterWindowManually();
+            this.Loaded -= OnWindowLoaded; // Remove event handler
+        }
+
+        private void CenterWindowManually()
+        {
+            var screen = Screens.Primary;
+            if (screen != null)
+            {
+                var screenWidth = screen.WorkingArea.Width;
+                var screenHeight = screen.WorkingArea.Height;
+                var screenX = screen.WorkingArea.X;
+                var screenY = screen.WorkingArea.Y;
+
+                // Get actual window size
+                var windowWidth = this.Width;
+                var windowHeight = this.Height;
+
+                // Calculate center position
+                var centerX = screenX + (screenWidth - windowWidth) / 2;
+                var centerY = screenY + (screenHeight - windowHeight) / 2;
+
+                Position = new PixelPoint((int)centerX, (int)centerY);
+            }
         }
     }
 }
