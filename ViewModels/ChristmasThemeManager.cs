2aafb6096ad20f4341cea499191950dd6f756cfc
Mathias Hansen <mathiashansen2007@gmail.com>
2025-10-02 19:28:35 +0200

    Added Christmas theme for winter #4

---
+++ ViewModels/ChristmasThemeManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AkademiTrack.Services
+{
+    public enum Season
+    {
+        None,
+        Christmas
+    }
+
+    public class ChristmasThemeManager : INotifyPropertyChanged
+    {
+        private static ChristmasThemeManager _instance;
+        private Season _currentSeason;
+
+        public static ChristmasThemeManager Instance => _instance ??= new ChristmasThemeManager();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ChristmasThemeManager()
+        {
+            UpdateCurrentSeason();
+        }
+
+        public Season CurrentSeason
+        {
+            get => _currentSeason;
+            private set
+            {
+                if (_currentSeason != value)
+                {
+                    _currentSeason = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(IsChristmas));
+                    OnPropertyChanged(nameof(ChristmasLightsVisible));
+                }
+            }
+        }
+
+        // Boolean properties for XAML binding
+        public bool IsChristmas => _currentSeason == Season.Christmas;
+
+        // Visibility helpers
+        public bool ChristmasLightsVisible => IsChristmas;
+
+        public void UpdateCurrentSeason()
+        {
+            var now = DateTime.Now;
+            var month = now.Month;
+            var day = now.Day;
+
+            // Christmas: December 1 - January 6
+            if ((month == 12) || (month == 1 && day <= 6))
+            {
+                CurrentSeason = Season.Christmas;
+            }
+            else
+            {
+                CurrentSeason = Season.None;
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
