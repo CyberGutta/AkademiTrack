ce96cb80892a667c49991a184020d87dbe8bd9dc
Andreas Nilsen <124470447+CyberNilsen@users.noreply.github.com>
2025-10-29 18:41:54 +0100

    Program now has mac auth, biometric and password works.

--- Services/MacOSAuthService.cs
+++ Services/MacOSAuthService.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.IO;
+using System;
 
 namespace AkademiTrack.Services
 {
@@ -6,8 +10,30 @@ namespace AkademiTrack.Services
     {
         public async Task<bool> AuthenticateAsync(string reason)
         {
-            await Task.Delay(100);
-            return true; 
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                return false;
+
+            var helperPath = Path.Combine(AppContext.BaseDirectory, "Assets", "Helpers", "AkademiAuth");
+
+            if (!File.Exists(helperPath))
+                return false;
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = helperPath,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            await process.WaitForExitAsync();
+
+            return process.ExitCode == 0;
         }
     }
 }
